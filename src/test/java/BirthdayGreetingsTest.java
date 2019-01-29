b82f4d8ff03ab9b8b06781cb929db5955bac192c
duq <duque@intre.it>
2019-01-29 11:25:13 +0100

    Added first test skeleton

--- src/test/java/BirthdayGreetingsTest.java
+++ src/test/java/BirthdayGreetingsTest.java
@@ -9,4 +9,24 @@ public class BirthdayGreetingsTest {
         assertEquals("a", "a");
     }
 
+    //This is an end to end test
+    @Test
+    void justAReference() {
+        //Start smtp server
+        //Prepare employee file
+
+        //BirthdayGreetings greetings = new BirthdayGreetings();
+        //greetings.send();
+            //Read form employees file
+            //Check for birthday
+            //Send mail through mail client
+
+        //read mail sent to smtp server
+        //assert on mail received
+
+        //Delete employee file
+        //Start smtp server
+    }
+
+
 }
