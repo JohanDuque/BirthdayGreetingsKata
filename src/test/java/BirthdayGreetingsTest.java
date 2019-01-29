79a0e238194e4106332b4402ac527bb2edf3f712
duq <duque@intre.it>
2019-01-29 13:06:16 +0100

    Server start stop implemented

--- src/test/java/BirthdayGreetingsTest.java
+++ src/test/java/BirthdayGreetingsTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Test;
 import support.LocalSmtpServer;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BirthdayGreetingsTest {
 
     //This is an end to end test
@@ -24,9 +28,19 @@ public class BirthdayGreetingsTest {
 
     @Test
     void sendMail() {
-        String mailHogBin = "support/MailHog_darwin_amd64";
-        LocalSmtpServer smtpServer = new LocalSmtpServer(mailHogBin, "127.0.0.1", 1025, 8025, 8025);
+        String mailHogBin = "/Users/duque/intr3/Kata/src/test/java/support/MailHog_darwin_amd64";
+        LocalSmtpServer smtpServer = new LocalSmtpServer(mailHogBin, "127.0.0.1", 1027, 8027, 8027);
 
-        smtpServer.sendMail("test@acme", "who@knows.com", "This is my subject", "this is the body of my message");
+        smtpServer.start();
+
+        try {
+            smtpServer.sendMail("test@acme", "who@knows.com", "This is my subject", "this is the body of my message");
+            final ArrayList<LocalSmtpServer.MailReceived> messages = smtpServer.currentState().getMessages();
+            assertEquals(1, messages.size());
+
+        } finally {
+            smtpServer.stop();
+        }
+
     }
 }
