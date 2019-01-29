37294d4dd9670b19340605a91684fd83e9635266
duq <duque@intre.it>
2019-01-29 12:30:15 +0100

    send mail modified to use support class

--- src/test/java/BirthdayGreetingsTest.java
+++ src/test/java/BirthdayGreetingsTest.java
@@ -1,11 +1,5 @@
 import org.junit.jupiter.api.Test;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import support.LocalSmtpServer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -36,21 +30,10 @@ public class BirthdayGreetingsTest {
     }
 
     @Test
-    void sendMail() throws MessagingException {
-        // Create a mail session
-        java.util.Properties props = new java.util.Properties();
-        props.put("mail.smtp.host", "127.0.0.1");
-        props.put("mail.smtp.port", "1025");
-        Session session = Session.getInstance(props, null);
+    void sendMail() {
+        String mailHogBin = "support/MailHog_darwin_amd64";
+        LocalSmtpServer smtpServer = new LocalSmtpServer(mailHogBin, "127.0.0.1", 1025, 8025, 8025);
 
-        // Construct the message
-        Message msg = new MimeMessage(session);
-        msg.setFrom(new InternetAddress("test@acme"));
-        msg.setRecipient(Message.RecipientType.TO, new InternetAddress("who@knows.com"));
-        msg.setSubject("Greetings");
-        msg.setText("Hello banana!");
-
-        // Send the message
-        Transport.send(msg);
+        smtpServer.sendMail("test@acme", "who@knows.com", "This is my subject", "this is the body of my message");
     }
 }
