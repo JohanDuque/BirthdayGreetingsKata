80e085362f70708fce9d99f4d5f53a357984bf6a
duq <duque@intre.it>
2019-01-29 11:46:39 +0100

    sedMail runner implemented

--- src/test/java/BirthdayGreetingsTest.java
+++ src/test/java/BirthdayGreetingsTest.java
@@ -1,5 +1,12 @@
 import org.junit.jupiter.api.Test;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BirthdayGreetingsTest {
@@ -28,5 +35,23 @@ public class BirthdayGreetingsTest {
         //Start smtp server
     }
 
+    @Test
+    void sendMail() throws MessagingException {
+        // Create a mail session
+        java.util.Properties props = new java.util.Properties();
+        props.put("mail.smtp.host", "127.0.0.1");
+        props.put("mail.smtp.port", "1025");
+        Session session = Session.getInstance(props, null);
 
+        // Construct the message
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("test@acme"));
+            msg.setRecipient(Message.RecipientType.TO, new InternetAddress("who@knows.com"));
+            msg.setSubject("Greetings");
+            msg.setText("Hello banana!");
+
+            // Send the message
+            Transport.send(msg);
+
+    }
 }
