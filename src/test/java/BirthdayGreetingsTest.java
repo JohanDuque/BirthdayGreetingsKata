3c9a12eb90154f95087834b12c07d10d0925f31f
duq <duque@intre.it>
2019-01-29 16:39:42 +0100

    House keeping..

--- src/test/java/BirthdayGreetingsTest.java
+++ src/test/java/BirthdayGreetingsTest.java
@@ -11,7 +11,6 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,38 +19,6 @@ public class BirthdayGreetingsTest {
     private LocalSmtpServer smtpServer;
     private static final String MAIL_HOG_BIN = "./src/test/java/support/MailHog_darwin_amd64";
     private final String filename = "employees_test.txt";
-
-    //This is an end to end test
-    @Test
-    void justAReference() {
-        //Start smtp server
-        //Prepare employee file
-
-        //BirthdayGreetings greetings = new BirthdayGreetings();
-        //greetings.send();
-        //Read form employees file
-        //Check for birthday
-        //Send mail through mail client
-
-        //read mail sent to smtp server
-        //assert on mail received
-
-        //Delete employee file
-        //Start smtp server
-    }
-
-    @Test
-    void prepareFile() throws IOException {
-        Files.write(Paths.get("employees.txt"), Arrays.asList(
-                "last_name, first_name, date_of_birth, email",
-                "Capone, Al, 1951-10-08, al.capone@acme.com",
-                "Escobar, Pablo, 1975-09-11, pablo.escobar@acme.com",
-                "Wick, John, 1987-09-11, john.wick@acme.com"
-        ));
-
-        List<String> lines = Files.readAllLines(Paths.get("employees.txt"));
-        lines.forEach(System.out::println);
-    }
 
     @BeforeEach
     void setUp() {
@@ -62,13 +29,6 @@ public class BirthdayGreetingsTest {
     @AfterEach
     void tearDown() {
         smtpServer.stop();
-    }
-
-    @Test
-    void sendMail() {
-        smtpServer.sendMail("test@acme", "who@knows.com", "This is my subject", "this is the body of my message");
-        final ArrayList<LocalSmtpServer.MailReceived> messages = smtpServer.currentState().getMessages();
-        assertEquals(1, messages.size());
     }
 
     @Test
