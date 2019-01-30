6a5f38e1d94c88b43e234687c9c1e7c3ff9adfa2
duq <duque@intre.it>
2019-01-30 17:25:00 +0100

    missing file test implemented

--- src/test/java/birthdaygreetingskata/CsvEmployeeFileRegistryTest.java
+++ src/test/java/birthdaygreetingskata/CsvEmployeeFileRegistryTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /*TEST LIST
@@ -43,6 +45,14 @@ class CsvEmployeeFileRegistryTest {
         assertEquals(3, allEmployees.size());
         assertTrue(allEmployees.contains(new Employee("Capone", "Al", "al.capone@acme.com", BirthDate.parse("1951-10-08"))));
         assertTrue(allEmployees.contains(new Employee("Escobar", "Pablo", "pablo.escobar@acme.com", BirthDate.parse("1975-09-11"))));
+    }
+
+    @Test
+    void missingFile() {
+        String missingFilename = "ThisFIleDoesNotExist";
+        CsvEmployeeFileRegistry csvEmployeeFileRegistry = new CsvEmployeeFileRegistry(missingFilename);
+
+        assertThrows(NoSuchFileException.class, () ->  csvEmployeeFileRegistry.getAllEmployees());
     }
 
     private void prepareFile(String filename, List<String> lines) throws IOException {
