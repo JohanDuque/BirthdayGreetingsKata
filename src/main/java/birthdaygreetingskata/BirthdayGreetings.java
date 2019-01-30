ed2c4a9611d448c0cb06061a96e7160b38371e4c
duq <duque@intre.it>
2019-01-30 16:05:49 +0100

    File parsing move to new EmployeeFactoryClass

--- src/main/java/birthdaygreetingskata/BirthdayGreetings.java
+++ src/main/java/birthdaygreetingskata/BirthdayGreetings.java
@@ -2,52 +2,26 @@ package birthdaygreetingskata;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class BirthdayGreetings {
 
-    private final String employeeFile;
+    private final EmployeeFactory employeeFactory = new EmployeeFactory();
     private SmtpPostalOffice smtpPostalOffice;
 
     BirthdayGreetings(String employeeFile, String host, int port) {
-        this.employeeFile = employeeFile;
+        this.employeeFactory.employeeFile = employeeFile;
         smtpPostalOffice = new SmtpPostalOffice(host, port);
     }
 
     void send(LocalDate today) throws MessagingException, IOException {
-        List<Employee> employees = parseEmployees(readEmployeeFile());
+        List<Employee> employees = employeeFactory.parseEmployees(employeeFactory.readEmployeeFile());
 
         for (Employee employee : employees) {
             if (employee.isBirthday(today)) {
                 smtpPostalOffice.sendMail(employee);
             }
         }
-    }
-
-    private List<Employee> parseEmployees(List<String> allLines) {
-        return allLines.stream().map(this::parseEmployee).collect(Collectors.toList());
-    }
-
-    private Employee parseEmployee(String employeeLine) {
-        List<String> employeePart = Arrays.stream(employeeLine.split(","))
-                .map(String::trim)
-                .collect(Collectors.toList());
-
-        return new Employee(
-                employeePart.get(0),
-                employeePart.get(1),
-                employeePart.get(3),
-                BirthDate.parse(employeePart.get(2)));
-    }
-
-    private List<String> readEmployeeFile() throws IOException {
-        return Files.readAllLines(Paths.get(employeeFile)).stream()
-                .skip(1)
-                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/birthdaygreetingskata/EmployeeFactory.java
@@ -0,0 +1,37 @@
+package birthdaygreetingskata;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EmployeeFactory {
+    String employeeFile;
+
+    public EmployeeFactory() {
+    }
+
+    List<Employee> parseEmployees(List<String> allLines) {
+        return allLines.stream().map(this::parseEmployee).collect(Collectors.toList());
+    }
+
+    Employee parseEmployee(String employeeLine) {
+        List<String> employeePart = Arrays.stream(employeeLine.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        return new Employee(
+                employeePart.get(0),
+                employeePart.get(1),
+                employeePart.get(3),
+                BirthDate.parse(employeePart.get(2)));
+    }
+
+    List<String> readEmployeeFile() throws IOException {
+        return Files.readAllLines(Paths.get(employeeFile)).stream()
+                .skip(1)
+                .collect(Collectors.toList());
+    }
+}
