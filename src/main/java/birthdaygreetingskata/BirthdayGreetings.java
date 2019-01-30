9c47778fe10f8038512b1f7f095688c64007757b
duq <duque@intre.it>
2019-01-30 15:49:14 +0100

    BirthdayGreetings refactor second step

--- src/main/java/birthdaygreetingskata/BirthdayGreetings.java
+++ src/main/java/birthdaygreetingskata/BirthdayGreetings.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,40 +14,38 @@ public class BirthdayGreetings {
     private final String employeeFile;
     private SmtpPostalOffice smtpPostalOffice;
 
-    public BirthdayGreetings(String employeeFile, String host, int port) {
+    BirthdayGreetings(String employeeFile, String host, int port) {
         this.employeeFile = employeeFile;
         smtpPostalOffice = new SmtpPostalOffice(host, port);
     }
 
-    public void send(LocalDate today) throws MessagingException, IOException {
-        // read from employee file
-        List<String> allLines = redEmployeeFile();
-        List<Employee> employees = new ArrayList<>();
-        // parse csv
-        for (String employeeLine : allLines) {
-            List<String> employeePart = Arrays.stream(employeeLine.split(","))
-                    .map(String::trim)
-                    .collect(Collectors.toList());
-
-            Employee employee = new Employee(
-                    employeePart.get(0),
-                    employeePart.get(1),
-                    employeePart.get(3),
-                    BirthDate.parse(employeePart.get(2)));
-
-            employees.add(employee);
-        }
+    void send(LocalDate today) throws MessagingException, IOException {
+        List<Employee> employees = parseEmployees(readEmployeeFile());
 
         for (Employee employee : employees) {
-            // check birthday
             if (employee.isBirthday(today)) {
                 smtpPostalOffice.sendMail(employee);
             }
         }
-
     }
 
-    private List<String> redEmployeeFile() throws IOException {
+    private List<Employee> parseEmployees(List<String> allLines) {
+        return allLines.stream().map(this::parseEmployee).collect(Collectors.toList());
+    }
+
+    private Employee parseEmployee(String employeeLine) {
+        List<String> employeePart = Arrays.stream(employeeLine.split(","))
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        return new Employee(
+                employeePart.get(0),
+                employeePart.get(1),
+                employeePart.get(3),
+                BirthDate.parse(employeePart.get(2)));
+    }
+
+    private List<String> readEmployeeFile() throws IOException {
         return Files.readAllLines(Paths.get(employeeFile)).stream()
                 .skip(1)
                 .collect(Collectors.toList());
